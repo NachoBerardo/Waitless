9949aea6e685b9b2be16a41af844da9f4e6e0410
NachoBerardo <nachoberardo2005@gmail.com>
2023-05-15 17:24:51 -0300

    a

--- waitless/src/pages/footer.tsx
+++ waitless/src/pages/footer.tsx
@@ -1,7 +1,9 @@
+import Image from 'next/image';
+
 export default function Footer() {
     return(
         <footer className="bg-gray-500 w-screen h-1/6 fixed bottom-0 ">
-            <button  className=""><img src="..\..\public\homeicon"/></button>
+            <button  className=""><img src="../../public/homeicon.jpg"/></button>
         </footer>
     )
 }
