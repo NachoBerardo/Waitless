aeabceb56d3ad88c6992cafc65e4d21acf7c3125
NachoBerardo <nachoberardo2005@gmail.com>
2023-05-16 11:24:23 -0300

    tratando de centrar boton

--- waitless/src/pages/footer.tsx
+++ waitless/src/pages/footer.tsx
@@ -3,14 +3,24 @@ import Image from 'next/image';
 export default function Footer() {
     return(
         <footer className="bg-gray-500 w-screen h-1/6 fixed bottom-0">
-            <button  className="translate-x-full">
-                <Image 
-                    src="/homeicon.jpg" 
-                    alt='Icono casa'
-                    width={50}
-                    height={50}
-                />
-            </button>
+            <div className="container min-w-full grid place-items-center">
+                <button  className="flex flex-col gap-8">
+                    <Image 
+                        src="/homeicon.jpg" 
+                        alt='Icono casa'
+                        width={50}
+                        height={50}
+                    />
+                </button>
+                <button  className="flex flex-col gap-8 -translate-y-full">
+                    <Image 
+                        src="/table.png" 
+                        alt='Icono mesa'
+                        width={50}
+                        height={50}
+                    />
+                </button>
+            </div>
         </footer>
     )
 }
