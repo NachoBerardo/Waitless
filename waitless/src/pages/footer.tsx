b866a3c7005a620a076a7e51db2946d83181a49e
NachoBerardo <nachoberardo2005@gmail.com>
2023-05-15 23:07:54 -0300

    foto

--- waitless/src/pages/footer.tsx
+++ waitless/src/pages/footer.tsx
@@ -2,8 +2,15 @@ import Image from 'next/image';
 
 export default function Footer() {
     return(
-        <footer className="bg-gray-500 w-screen h-1/6 fixed bottom-0 ">
-            <button  className=""><img src="../../public/homeicon.jpg"/></button>
+        <footer className="bg-gray-500 w-screen h-1/6 fixed bottom-0">
+            <button  className="translate-x-full">
+                <Image 
+                    src="/homeicon.jpg" 
+                    alt='Icono casa'
+                    width={50}
+                    height={50}
+                />
+            </button>
         </footer>
     )
 }
