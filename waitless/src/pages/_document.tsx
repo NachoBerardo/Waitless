36dd18ff527ad5e80a51bdb47cd75c2cb1830b63
NachoBerardo <nachoberardo2005@gmail.com>
2023-05-18 14:24:26 -0300

    fondo arreglado

--- waitless/src/pages/_document.tsx
+++ waitless/src/pages/_document.tsx
@@ -2,15 +2,16 @@ import { Html, Head, Main, NextScript } from 'next/document'
 import  Footer  from './footer'
 import  Header  from './header'
 
-export default function Document() {
+export default function document() {
   return (
     <Html lang="en">
       <Header/>
-      <Head/>
-      <body className=""> 
-        <Main/> 
+      <body className="w-screen h-screen"> 
+        <Main /> 
+        <div className="bg-footer"></div>
         <NextScript />
       </body>
+      <Head />
       <Footer/>
     </Html>
   )
