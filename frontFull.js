61847d5d69e4a1d6efb82405f7531afcb088bbce
VELO BARES ABRIL <47206356@ort>
2023-06-15 14:25:20 -0300

    fetch

---
+++ frontFull.js
@@ -0,0 +1,8 @@
+//FETCH
+
+
+async function logJSONData() {
+    const response = await fetch("http://example.com/movies.json");
+    const jsonData = await response.json();
+    console.log(jsonData);
+  }
