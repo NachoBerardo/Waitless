bff86c8fc90bfe2ee6c0d7b12cd605c4079ba0a7
NachoBerardo <nachoberardo2005@gmail.com>
2023-08-04 10:25:23 -0300

    a

--- waitless/src/pages/restaurante.tsx
+++ waitless/src/pages/restaurante.tsx
@@ -1,6 +1,12 @@
-export default function PantallaRestaurante(){
-    return(
-        <main>
+import { useState } from "react";
+
+const PantallaRestaurante:React.FC = () => {
+    const [rotation, setRotation] = useState(false);
+    const handleClickRotation = () => {
+        setRotation(!rotation);
+      };
+
+    return <main>
             <div className="w-screen h-screen bg-white">
                 <header className="h-fit w-full flex shadow-lg items-center">
                     <div className="pl-8 flex items-center h-full w-fit py-7">
@@ -19,13 +25,27 @@ export default function PantallaRestaurante(){
 
                     </img>
                 </header>
-                <div className="ml-20 mt-10 py-3 pl-7 w-fit h-fit left-0 border-solid border-2 rounded-[10px] flex items-center border-BordeGrisPedido">
-                    <h5 className="text-black ">Filtrar</h5>
-                    <button>
-                        <img src="arrow-up.svg" alt="" className="rotate-180 w-4 h-4 ml-4 mr-7" />
-                    </button>
+                <div className="ml-20 mt-10 py-3 pl-7 w-fit h-fit left-0 border-solid border-2 rounded-[10px] flex flex-col items-center border-BordeGrisPedido">
+                    <div className="flex items-center">
+                        <h5 className="text-black item-">Filtrar</h5>
+                        <button
+                            style={{ transform: `rotate(${rotation ? "180deg" : "0deg"})` }}
+                            onClick={handleClickRotation}
+                            className={"h-[16px] w-[16px] ml-4 mr-7 transform focus:rotate-0 transition-transform"}
+
+                        >
+                            <img src="arrow-up.svg" alt=""  />
+                        </button>
+                    </div>
                 </div>
+                {rotation ? (
+                    <></>
+                ) : (
+                    <div className="ml-20 top-0 h-4 w-4 bg-footer"></div>
+                )}
             </div>
-        </main>
-    )
+        </main>;
+    
 }
+
+export default PantallaRestaurante
