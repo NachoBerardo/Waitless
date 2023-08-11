08066b401f04a16e50e561bd509d247b0a954f87
NachoBerardo <nachoberardo2005@gmail.com>
2023-08-11 10:16:21 -0300

    a

--- waitless/src/pages/restaurante.tsx
+++ waitless/src/pages/restaurante.tsx
@@ -2,6 +2,11 @@ import { useState } from "react";
 import DropDownRestaurante from "../components/DropDownRestaurante";
 
 const PantallaRestaurante:React.FC = () => {
+    const [rotation, setRotation] = useState(false);
+        const handleClickRotation = () => {
+            setRotation(!rotation);
+          };
+
     return <main>
             <div className="w-screen h-screen bg-white">
                 <header className="h-fit w-full flex shadow-lg items-center">
@@ -22,20 +27,48 @@ const PantallaRestaurante:React.FC = () => {
                     </img>
                 </header>
                 <DropDownRestaurante/>
-                <div className="flex flex-row justify-around pt-11 z-10">
-                    <div className="h-fit w-full ml-20 mr-6 border-4 rounded-[10px] border-solid border-BorderPedidosRestaurante">
+                <div className="grid grid-cols-3  justify-around m-auto pt-11 z-10">
+                    <div className="h-fit  ml-20 mr-6 border-4 rounded-[10px] border-solid border-BorderPedidosRestaurante">
                         <h4 className="text-BorderPedidosRestaurante pl-5 pt-5 pb-11">Pendientes</h4>
-                        <div className="w-full h-fit mx-7 bg-RojoPedido flex justify-around">
+                        <div className=" mx-7 mb-5 bg-RojoPedido rounded-[10px] flex justify-around items-center">
                             <h5 className="py-4 text-black">#1</h5>
                             <h5 className="py-4 text-black">00:00hs</h5>
-                            <h5 className="py-4 text-black">a</h5>
+                            <button
+                                style={{ transform: `rotate(${rotation ? "0deg" : "180deg"})` }}
+                                onClick={handleClickRotation}
+                                className={"h-[16px] w-[16px] transform focus:rotate-0 transition-transform "}
+                            >
+                                <img src="arrow-up.svg" alt="" />
+                            </button>
                         </div>
                     </div>
-                    <div className="h-20 w-full mr-6 border-4 rounded-[10px] border-solid border-BorderPedidosRestaurante">
-
+                    <div className="h-fit mr-6 border-4 rounded-[10px] border-solid border-BorderPedidosRestaurante">
+                        <h4 className="text-BorderPedidosRestaurante pl-5 pt-5 pb-11">En Proceso</h4>
+                        <div className=" mx-7 mb-5 bg-[#d29b2ecc] rounded-[10px] flex justify-around items-center">
+                            <h5 className="py-4 text-black">#1</h5>
+                            <h5 className="py-4 text-black">00:00hs</h5>
+                            <button
+                                style={{ transform: `rotate(${rotation ? "0deg" : "180deg"})` }}
+                                onClick={handleClickRotation}
+                                className={"h-[16px] w-[16px] transform focus:rotate-0 transition-transform "}
+                            >
+                                <img src="arrow-up.svg" alt="" />
+                            </button>
+                        </div>
                     </div>
-                    <div className="h-20 w-full mr-20 border-4 rounded-[10px] border-solid border-BorderPedidosRestaurante">
-
+                    <div className="h-fit  mr-20 border-4 rounded-[10px] border-solid border-BorderPedidosRestaurante">
+                        <h4 className="text-BorderPedidosRestaurante pl-5 pt-5 pb-11">Completados</h4>
+                        <div className=" mx-7 mb-5 bg-[#00B493] rounded-[10px] flex justify-around items-center">
+                            <h5 className="py-4 text-black">#1</h5>
+                            <h5 className="py-4 text-black">00:00hs</h5>
+                            <button
+                                style={{ transform: `rotate(${rotation ? "0deg" : "180deg"})` }}
+                                onClick={handleClickRotation}
+                                className={"h-[16px] w-[16px] transform focus:rotate-0 transition-transform "}
+                            >
+                                <img src="arrow-up.svg" alt="" />
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
