f76116b08b0c2084d73dca329f75ec1e030374f1
NachoBerardo <nachoberardo2005@gmail.com>
2023-08-21 15:50:11 -0300

    a

--- waitless/src/components/Pedidos.tsx
+++ waitless/src/components/Pedidos.tsx
@@ -1,18 +1,42 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+
 interface Props { 
-    pedidos: string[][];
+    pedidos: string[];
+    id:number;
+    hora:string;
     color:keyof ColorVariants;
-    key: number; 
+    key: number;
+    pedidoCompleto: TipoPedido; 
   }
+  type TipoPedido= {
+    pedidos: string[];
+    hora: string;
+    id: number;
+}
 
   interface ColorVariants {
     verde: string[];
     amarillo: string[];
     rojo: string[];
 }
-let color = "verde";
 
-const Pedidos: React.FC<Props> = ({ pedidos, color, key}) => {
+const Pedidos: React.FC<Props> = ({ pedidos, color, key, id, hora, pedidoCompleto}) => {
+//-------------------------Constantes de dragg----------------------------- https://www.youtube.com/watch?v=Q1PYQPK9TaM&ab_channel=asat
+    const dragItem = useRef<[string[], number, string] | null>(null);
+    const dragNode = useRef<EventTarget  | null>(null);
+    
+    const [dragging, setDragging] = useState(false);
+
+    const handleDragStart = (e: React.SyntheticEvent<EventTarget>, pedidos: string[], id:number, hora:string, ) =>{
+        console.log(pedidos, id, hora)
+        dragItem.current = [pedidos, id, hora];
+        dragNode.current = e.currentTarget;
+    }
+
+//-------------------------Constantes de rotacion y colores-----------------------------
+
     const [rotation, setRotation] = useState(false);
     const handleClickRotation = () => {
         setRotation(!rotation);
@@ -22,33 +46,34 @@ const Pedidos: React.FC<Props> = ({ pedidos, color, key}) => {
         amarillo: ['bg-[#D29B2ECC]', 'bg-[#d29b2e40]'],
         rojo: ['bg-RojoPedido', 'bg-[#d2382e33]'],
     }
-
-    return <div className={`grid mb-5 rounded-[10px] mx-10`}>
-    <div className={` ${colorvariants[color][0]} flex justify-around items-center`}>
-        <h5 className="py-4 text-black">#{pedidos[0]}</h5>
-        <h5 className="py-4 text-black">{pedidos[1]}hs</h5>
-        <button
-            style={{ transform: `rotate(${rotation ? "0deg" : "180deg"})` }}
-            onClick={handleClickRotation}
-            className={"h-[16px] w-[16px] transform focus:rotate-0 transition-transform "}
-        >
-            <img src="arrow-up.svg" alt="" />
-        </button>
-    </div>
-    {rotation ? (
-        <div className="grid">
-            {pedidos.map((index, key)=>(
-            <div className={`z-20 px-4 py-1 flex justify-between items-center ${colorvariants[color][1]} border-t border-black w-full`}>
-                <p className="text-black">{pedidos[index][2][index]}</p>
-                <p className="text-black">1000</p>
-            </div>
-            ))}
-            
+    
+    return <div draggable={true} onDragStart={(e)=>{handleDragStart(e, pedidos, id, hora)}} className={`grid mb-5 rounded-[10px] mx-10 cursor-pointer `}>
+        <div className={` ${colorvariants[color][0]} flex justify-around items-center`}>
+            <h5 className="py-4 text-black">#{id}</h5>
+            <h5 className="py-4 text-black">{hora}hs</h5>
+            <button
+                style={{ transform: `rotate(${rotation ? "0deg" : "180deg"})` }}
+                onClick={handleClickRotation}
+                className={"h-[16px] w-[16px] transform focus:rotate-0 transition-transform "}
+            >
+                <img src="arrow-up.svg" alt="" />
+            </button>
         </div>
-        
-    ) : (
-        <></>
-    )}
+        {rotation ? (
+            <div className="grid">
+                {pedidos.map((pedido, key)=>(
+                <div className={`z-20 px-4 py-1 flex justify-between items-center ${colorvariants[color][1]} border-t border-black w-full`}>
+                    <p className="text-black">{pedido}</p>
+                    <p className="text-black">1000</p>
+                </div>
+                ))}
+                
+            </div>
+            
+        ) : (
+            <></>
+        )}
     </div>
+        
 }
 export default Pedidos
--- waitless/src/pages/restaurante.tsx
+++ waitless/src/pages/restaurante.tsx
@@ -1,19 +1,45 @@
 import { useState } from "react";
 import Pedidos from "../components/Pedidos";
 import DropDownRestaurante from "../components/DropDownRestaurante";
+import { DndContext, closestCenter } from "@dnd-kit/core";
+import { SortableContext, verticalListSortingStrategy } from "@dnd-kit/sortable";
 
+type TipoPedido= {
+    pedidos: string[];
+    hora: string;
+    id: number;
+}
+const handleDragEnd=()=>{}
+  
 const PantallaRestaurante:React.FC = () => {
-    
+    const [pedidosPendientes, setPedidosPendiente] = useState<TipoPedido[]>([
+        { pedidos: ['Empanada', 'Churrasco'], hora: '12:15', id: 1 },
+        { pedidos: ["Empanada", "Churrasco", "Provoleta", "a", "Volcan de chocolate con helado"], hora: '12:20', id: 2 },
+        { pedidos: ["Carne", "Churrasco"], hora: '12:25', id: 3 },
+        { pedidos: ["Empanada", "Carne"], hora: '12:30', id: 4 },
+        { pedidos: ["Empanada", "Flan"], hora: '12:35', id: 5 },
+        { pedidos: ["Agua"], hora: '12:40', id: 6 },
+        { pedidos: ["Empanada", "Vegano"], hora: '12:45', id: 7 },
+        { pedidos: ["Provoleta", "Churrasco"], hora: '12:50', id: 8 },
+        { pedidos: ["Empanada", "Churrasco", "Provoleta", "a", "Volcan de chocolate con helado", "Ensalada de papa y huevo"], hora: '12:55', id: 9 },
+    ]);
 
-    const pedidos = [
-        [["1"], ["12:15"], ["Empanada", "Churrasco"]],[["2"],["12:20"],["Empanada", "Churrasco", "Provoleta", "a", "Volcan de chocolate con helado"]],[["3"],["12:25"],["Empanada", "Churrasco"]],[["4"],["12:30"],["Empanada", "Churrasco"]],[["5"],["12:30"],["Empanada", "Churrasco"]],[["6"],["12:30"],["Empanada", "Churrasco"]],[["7"],["12:30"],["Empanada", "Churrasco"]],[["8"],["12:30"],["Empanada", "Churrasco"]],[["8"],["12:30"],["Empanada", "Churrasco"]],[["8"],["12:30"],["Empanada", "Churrasco"]]
-    ];
+    const [pedidosEnproceso, setpedidosEnprocesoe] = useState<TipoPedido[]>([
+        { pedidos: ['Provoleta", "Papas fritas'], hora: '1:15', id: 1 },
+        { pedidos: ["Coca", "Helado", "Ensalada de fruta", "Budin de pan", "Volcan de chocolate con helado y dulce de leche aaaaaa"], hora: '1:20', id: 2 },
+        { pedidos: ["Provoleta", "Churrasco"], hora: '1:25', id: 3 },
+    ]);
+
+    const [pedidosCompletados, setpedidosCompletados] = useState<TipoPedido[]>([
+        { pedidos: ["Pure", "Milanesa"], hora: '2:15', id: 1 },
+        { pedidos: ["pollo", "Pasto"], hora: '2:20', id: 2 },
+        { pedidos: ["Papas fritas", "Carne al horno"], hora: '2:25', id: 3 },
+        { pedidos: ["a", "b"], hora: '2:30', id: 4 },
+
+    ]);
     const[pendientes, setPendientes] = useState(true);
     const[enProceso, setEnProceso] = useState(true);
     const[completados, setCompletados] = useState(true);
-
-   
-
     return <main className="no-scrollbar">
             <div className="w-full min-h-screen h-full bg-white no-scrollbar">
                 <header className="h-fit w-full flex shadow-lg items-center">
@@ -34,48 +60,48 @@ const PantallaRestaurante:React.FC = () => {
                     </img>
                 </header>
                 <DropDownRestaurante setPendientes={setPendientes} setCompletados={setCompletados} setEnProceso={setEnProceso}/>
-
+                
                 <div className="grid grid-cols-3 justify-around m-auto monitor:pt-6 xl:pt-11 z-10 ">
-                    {pendientes ?(
-                        <div className="custombp:h-[660px] notebook:h-[570px] ml-20 border-4 rounded-[10px] border-solid border-BorderPedidosRestaurante overflow-scroll no-scrollbar">
-                        <h4 className="text-BorderPedidosRestaurante pl-5 pt-5 pb-11">Completados</h4>
-                        {pedidos.map((pedidos, key)=>(
-                            <div key={key}>
-                              <Pedidos pedidos={pedidos} color={"rojo"} key={key}></Pedidos>
+                    {completados ?(
+                            <div className="custombp:h-[660px] notebook:h-[570px] ml-20 border-4 rounded-[10px] border-solid border-BorderPedidosRestaurante overflow-scroll no-scrollbar">
+                            <h4 className="text-BorderPedidosRestaurante pl-5 pt-5 pb-11">Completados</h4>
+                            {pedidosCompletados.map((pedidosMap, key)=>(
+                                    <div key={key}>
+                                        <Pedidos pedidoCompleto={pedidosMap} pedidos={pedidosMap.pedidos} id={pedidosMap.id} hora={pedidosMap.hora} color={"rojo"} key={key}></Pedidos>
+                                    </div>
+                            ))}
                             </div>
-                        ))}
-                        
-                    </div>
+
+
                     ):(
                         <></>
                     )}
                     {enProceso ?(
                         <div className="custombp:h-[660px] notebook:h-[570px] ml-6 mr-6 border-4 rounded-[10px] border-solid border-BorderPedidosRestaurante overflow-scroll no-scrollbar">
                         <h4 className="text-BorderPedidosRestaurante pl-5 pt-5 pb-11">En Proceso</h4>
-                        {pedidos.map((pedidos, key)=>(
-                            <div key={key}>
-                                <Pedidos pedidos={pedidos} color={"amarillo"} key={key}></Pedidos>
-                            </div>
+                        {pedidosEnproceso.map((pedidosMap, key)=>(
+                                <div key={key}>
+                                    <Pedidos pedidoCompleto={pedidosMap} pedidos={pedidosMap.pedidos} id={pedidosMap.id} hora={pedidosMap.hora} color={"amarillo"} key={key}></Pedidos>
+                                </div>
                         ))}
-                        
-                    </div>
+                        </div>
                     ):(
                         <></>
                     )}
-                    {completados ?(
+                    {pendientes ?(
                         <div className="custombp:h-[660px] notebook:h-[570px] mr-20 border-4 rounded-[10px] border-solid border-BorderPedidosRestaurante overflow-scroll no-scrollbar">
                         <h4 className="text-BorderPedidosRestaurante pl-5 pt-5 pb-11">Pendientes</h4>
-                        {pedidos.map((pedidos, key)=>(
-                            <div key={key}>
-                               <Pedidos pedidos={pedidos} color={"verde"} key={key}></Pedidos>   
-                            </div>    
+                        {pedidosPendientes.map((pedidosMap, key)=>(
+                                <div key={key}>
+                                    <Pedidos pedidoCompleto={pedidosMap} pedidos={pedidosMap.pedidos} id={pedidosMap.id} hora={pedidosMap.hora} color={"verde"} key={key}></Pedidos>   
+                                </div>    
                         ))}
-                        
-                    </div>
+                        </div>
                     ):(
                         <></>
                     )}
                 </div>
+               
                 <footer className="absolute bottom-0 w-full h-fit flex justify-center items-center pb-3 pt-5">
                     <button>
                       <img src="tacho.svg" alt="" className="w-16 h-16"/>
