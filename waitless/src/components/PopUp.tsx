ea520902e9cd7c64dc9d501672c3034ad8df90dd
ValentinaHsu <valentina.hsu@gmail.com>
2023-10-12 11:56:27 -0300

    terminado el popuo

--- waitless/src/components/PopUp.tsx
+++ waitless/src/components/PopUp.tsx
@@ -17,6 +17,7 @@ const PopUp: React.FC<Props> = ({ combinedArray, arrayUsed, keyPlato, setShowPop
   const handleClickRotation = () => {
     setRotation(!rotation);
   };
+  const sideDish = combinedArray[arrayUsed][keyPlato].sideDish;
 
   return <div className="h-screen w-screen pb-[7px] bg-background_popup overflow-x-hidden no-scrollbar">
     <HeaderPlato url={combinedArray[arrayUsed][keyPlato].image} setShowPopUP={setShowPopUP} setShowMenu={setShowMenu} />
@@ -29,6 +30,29 @@ const PopUp: React.FC<Props> = ({ combinedArray, arrayUsed, keyPlato, setShowPop
       </p>
       <p className="text-black px-4 pt-1 font-bold">${combinedArray[arrayUsed][keyPlato].price}</p>
     </div>
+
+    {sideDish ? (
+      <div className="w-screen h-fit pb-4 bg-background overflow-scroll mt-2 relative drop-shadow-md">
+        <h4 className="text-black pt-4 px-4 w-fit">Guarnicion</h4>
+        <button
+          style={{ transform: `rotate(${rotation ? "180deg" : "0deg"})` }}
+          className={"h-[16px] w-[16px] absolute top-4 right-7 transform focus:rotate-0 transition-transform"}
+          onClick={handleClickRotation}
+        >
+          <img src="arrow-up.svg" alt="" />
+        </button>
+        <h6 className="text-populetter pb-4 px-4 font-normal">
+          Elija 1 opción
+        </h6>
+        {rotation ? (
+          acompañamientos()
+
+        ) : (
+          <></>
+        )}
+      </div>) : (<></>)}
+
+
 
     <div className="w-screen h-fit pb-4 mb-24 bg-background overflow-scroll mt-2 relative drop-shadow-md">
       <h4 className="text-black px-4 pt-4 pb-3">¿Tenes alguna especificacion?</h4>
